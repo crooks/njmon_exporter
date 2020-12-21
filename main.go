adbac0961220a71a99ab7c9c8d477d982ac0563d
Steve Crook <steve@stmellion.org>
2020-12-21 11:20:26 +0000

    Call listener function directly from main()

--- main.go
+++ main.go
@@ -6,14 +6,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func recordMetrics() {
-	go func() {
-		for {
-			listener()
-		}
-	}()
-}
-
 func main() {
 	go listener()
 	http.Handle("/metrics", promhttp.Handler())
